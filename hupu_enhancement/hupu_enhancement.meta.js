d9318ad2310dcadd4589d068e11348daefdcb519
zhaeong97 <165234020+zhaeong97@users.noreply.github.com>
2025-12-02 18:23:05 +0800

    Refactor metadata in hupu_enhancement.meta.js
    Updated metadata for Hupu Enhancement script, including namespace, descriptions, license, and URLs.

--- hupu_enhancement/hupu_enhancement.meta.js
+++ hupu_enhancement/hupu_enhancement.meta.js
@@ -2,15 +2,12 @@
 // @name         虎扑增强
 // @name:en      Hupu Enhancement
 // @name:zh-CN   虎扑增强
-// @namespace    http://tampermonkey.net/
+// @namespace    https://github.com/zhaeong97/
 // @version      1.4.1
 // @description  虎扑帖子图片优化、视频悬浮、评论交互增强、工具栏优化等多项功能，提升虎扑浏览体验
 // @description:zh-CN  虎扑帖子图片优化、视频悬浮、评论交互增强、工具栏优化等多项功能，提升虎扑浏览体验
-// @description:en Hu Pu post image optimization, video suspension, comment interaction enhancement, toolbar optimization and other functions enhance the Hu Pu browsing experience
+// @description:en Hupu posts, image optimization, video hover effects, enhanced comment interactions, toolbar improvements, and more features to elevate your Hupu browsing experience.
 // @author       zhaeong
-// @homepage     https://github.com/zhaeong97/hupu-enhancement
-// @supportURL   https://github.com/zhaeong97/hupu-enhancement/issues
-// @license      MIT
 // @match        https://bbs.hupu.com/*
 // @match        https://*.hupu.com/*
 // @icon         https://w1.hoopchina.com.cn/images/pc/old/favicon.ico
@@ -23,7 +20,13 @@
 // @grant        GM_getValue
 // @grant        GM_getResourceText
 // @grant        GM_registerMenuCommand
+// @license      AGPL-3.0-or-later
+// @licenseURL   https://www.gnu.org/licenses/agpl-3.0.html
 // @noframes
-// @downloadURL https://github.com/zhaeong97/tampermonkey/raw/refs/heads/main/hupu_enhancement/hupu-enhancement.user.js
-// @updateURL https://github.com/zhaeong97/tampermonkey/raw/refs/heads/main/hupu_enhancement/hupu_enhancement.meta.js
+// @note         25-12-02 1.4.1 优化视频悬浮窗功能并增强错误处理
+// @note         25-12-02 1.4.0 初版发布（本地反复打磨，4个功能模块，因此v1.4.0）
+// @homepage     https://github.com/zhaeong97/tampermonkey
+// @supportURL   https://github.com/zhaeong97/tampermonkey/issues
+// @downloadURL  https://github.com/zhaeong97/tampermonkey/raw/refs/heads/main/hupu_enhancement/hupu-enhancement.user.js
+// @updateURL    https://github.com/zhaeong97/tampermonkey/raw/refs/heads/main/hupu_enhancement/hupu_enhancement.meta.js
 // ==/UserScript==
