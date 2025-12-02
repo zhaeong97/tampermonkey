4bf9bae4a110dbe2a3f121d7e2902cfac2b157df
zhaeong97 <165234020+zhaeong97@users.noreply.github.com>
2025-12-02 17:52:51 +0800

    创建元数据文件

---
+++ hupu_enhancement/hupu_enhancement.meta.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name         虎扑增强
+// @name:en      Hupu Enhancement
+// @name:zh-CN   虎扑增强
+// @namespace    http://tampermonkey.net/
+// @version      1.4.1
+// @description  虎扑帖子图片优化、视频悬浮、评论交互增强、工具栏优化等多项功能，提升虎扑浏览体验
+// @description:zh-CN  虎扑帖子图片优化、视频悬浮、评论交互增强、工具栏优化等多项功能，提升虎扑浏览体验
+// @description:en Hu Pu post image optimization, video suspension, comment interaction enhancement, toolbar optimization and other functions enhance the Hu Pu browsing experience
+// @author       zhaeong
+// @homepage     https://github.com/zhaeong97/hupu-enhancement
+// @supportURL   https://github.com/zhaeong97/hupu-enhancement/issues
+// @license      MIT
+// @match        https://bbs.hupu.com/*
+// @match        https://*.hupu.com/*
+// @icon         https://w1.hoopchina.com.cn/images/pc/old/favicon.ico
+// @icon64       https://w1.hoopchina.com.cn/images/pc/old/favicon.ico
+// @require      https://apps.bdimg.com/libs/jquery/2.1.4/jquery.min.js
+// @resource     layuiCss https://unpkg.com/layui@2.9.14/dist/css/layui.css
+// @resource     layuiJs https://unpkg.com/layui@2.9.14/dist/layui.js
+// @grant        GM_addStyle
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_getResourceText
+// @grant        GM_registerMenuCommand
+// @noframes
+// @downloadURL https://update.greasyfork.org/scripts/557606/%E8%99%8E%E6%89%91%E5%A2%9E%E5%BC%BA.user.js
+// @updateURL https://update.greasyfork.org/scripts/557606/%E8%99%8E%E6%89%91%E5%A2%9E%E5%BC%BA.meta.js
+// ==/UserScript==
